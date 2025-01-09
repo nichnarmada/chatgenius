e3949782e2eae083f5030805aea53e8bf1c30e20
Nicholas Narmada <nichnarmada@gmail.com>
2025-01-08 19:35:04 -0500

    fixed middleware accessing page without session

--- middleware.ts
+++ middleware.ts
@@ -39,7 +39,9 @@ export async function middleware(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
+    !request.nextUrl.pathname.startsWith("/auth") &&
+    !request.nextUrl.pathname.startsWith("/sign-up") &&
+    !request.nextUrl.pathname.startsWith("/forgot-password")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone()
