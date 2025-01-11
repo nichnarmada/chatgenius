f37675d0e4d7624832589e1e782423c28bc64d11
Nicholas Narmada <nichnarmada@gmail.com>
2025-01-11 15:00:41 -0500

    fixed lint errors

--- app/(auth-pages)/login/login-form.tsx
+++ app/(auth-pages)/login/login-form.tsx
@@ -71,7 +71,7 @@ export function LoginForm({ messageParams }: LoginFormProps) {
         <FormMessage message={messageParams} />
 
         <p className="text-center text-sm text-muted-foreground">
-          Don't have an account?{" "}
+          Don&apos;t have an account?{" "}
           <Link
             href="/sign-up"
             className="font-medium text-primary hover:underline"
